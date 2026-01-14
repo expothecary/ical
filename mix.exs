42f69c3eb989f16103f5a2bad9dcc16c5722a6c8
Aaron Seigo <aseigo@kde.org>
2026-01-14 18:39:04 +0100

    add credo, dialyzer, coveralls and a basic ci workflow

--- mix.exs
+++ mix.exs
@@ -14,15 +14,30 @@ defmodule ICalendar.Mixfile do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs(),
-      package: package()
+      package: package(),
+      preferred_cli_env: cli(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
   defp deps do
     [
       {:timex, "~> 3.4"},
-      {:mix_test_watch, ">= 0.0.0", only: :dev},
+      {:mix_test_watch, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.14", only: :test, runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.github": :test
+      ]
     ]
   end
 
