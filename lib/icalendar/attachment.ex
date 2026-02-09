f54fb98211a10c2f757d91b14481ab2c180946db
Aaron Seigo <aseigo@yaybr.com>
2026-02-09 16:53:52 +0100

    add a decode data helper

--- lib/icalendar/attachment.ex
+++ lib/icalendar/attachment.ex
@@ -7,4 +7,13 @@ defmodule ICalendar.Attachment do
           data: String.t(),
           mimetype: String.t() | nil
         }
+
+  def decoded_data(%__MODULE__{data_type: :base64, data: data}) do
+    case Base.decode64(data) do
+      :error -> Base.decode64(data, padding: false)
+      decoded -> decoded
+    end
+  end
+
+  def decoded_data(%__MODULE__{data: data}), do: data
 end
