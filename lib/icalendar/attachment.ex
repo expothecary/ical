8f604778ff5d1539802200fe09c865d92fb06894
Aaron Seigo <aseigo@yaybr.com>
2026-02-09 17:34:09 +0100

    add some documentation

--- lib/icalendar/attachment.ex
+++ lib/icalendar/attachment.ex
@@ -1,4 +1,8 @@
 defmodule ICalendar.Attachment do
+  @moduledoc """
+  An ICal attachment entry. CID, URI, and binary attachments are supported.
+  """
+
   @enforce_keys [:data_type, :data]
   defstruct [:data_type, :data, mimetype: nil]
 
@@ -8,6 +12,14 @@ defmodule ICalendar.Attachment do
           mimetype: String.t() | nil
         }
 
+  @spec decoded_data(t()) :: String.t() | binary
+  @doc """
+  Returns the decoded data for an Attachment.
+
+  For CID and URI attachments, this is simply the string representing the resource.
+
+  For inline binary attachments, this is decoded based on its advertised encoding.
+  """
   def decoded_data(%__MODULE__{data_type: :base64, data: data}) do
     case Base.decode64(data) do
       :error -> Base.decode64(data, padding: false)
