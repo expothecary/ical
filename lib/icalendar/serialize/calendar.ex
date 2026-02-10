5490c7b677e0795a0dd6f2dbb140a9e97c585b00
Aaron Seigo <aseigo@yaybr.com>
2026-02-10 11:35:54 +0100

    move calendar serialization into its own file

---
+++ lib/icalendar/serialize/calendar.ex
@@ -0,0 +1,33 @@
+defimpl ICalendar.Serialize, for: ICalendar do
+  def to_ics(calendar) do
+    []
+    |> start_calendar(calendar)
+    |> scale(calendar)
+    |> version(calendar)
+    |> product_id(calendar)
+    |> method(calendar)
+    |> events(calendar)
+    |> end_calendar(calendar)
+  end
+
+  defp start_calendar(acc, _calendar), do: acc ++ ["BEGIN:VCALENDAR\n"]
+  defp end_calendar(acc, _calendar), do: acc ++ ["END:VCALENDAR\n"]
+
+  defp scale(acc, %{scale: nil}), do: acc
+  defp scale(acc, calendar), do: acc ++ ["CALSCALE:", calendar.scale, "\n"]
+
+  defp method(acc, %{method: nil}), do: acc
+  defp method(acc, calendar), do: acc ++ ["METHOD:", calendar.method, "\n"]
+
+  defp version(acc, %{version: nil}), do: acc
+  defp version(acc, calendar), do: acc ++ ["VERSION:", calendar.version, "\n"]
+
+  defp product_id(acc, %{product_id: nil}), do: acc
+  defp product_id(acc, calendar), do: acc ++ ["PRODID:", calendar.product_id, "\n"]
+
+  defp events(acc, %{events: []}), do: acc
+
+  defp events(acc, calendar) do
+    acc ++ Enum.map(calendar.events, &ICalendar.Serialize.to_ics/1)
+  end
+end
