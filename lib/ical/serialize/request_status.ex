7283cc3334abc323529baa6e7b99aea09dbf5b6f
Aaron Seigo <aseigo@mykolab.com>
2026-02-20 21:23:39 +0100

    share geo field serialization

---
+++ lib/ical/serialize/request_status.ex
@@ -0,0 +1,28 @@
+defmodule ICal.Serialize.RequestStatus do
+  @moduledoc false
+
+  alias ICal.Serialize
+
+  def to_ics(%ICal.RequestStatus{} = request_status) do
+    [
+      "REQUEST-STATUS",
+      params(request_status),
+      code(request_status),
+      description(request_status),
+      exception(request_status),
+      ?\n
+    ]
+  end
+
+  defp params(%ICal.RequestStatus{language: nil}), do: ?:
+  defp params(%ICal.RequestStatus{language: language}), do: [";LANGUAGE=", language, ?:]
+
+  defp code(%ICal.RequestStatus{code: {x, y}}), do: "#{x}.#{y}"
+  defp code(%ICal.RequestStatus{code: {x, y, z}}), do: "#{x}.#{y}.#{z}"
+
+  defp description(%ICal.RequestStatus{description: nil}), do: ""
+  defp description(%ICal.RequestStatus{description: desc}), do: [?;, Serialize.to_ics(desc)]
+
+  defp exception(%ICal.RequestStatus{exception: nil}), do: ""
+  defp exception(%ICal.RequestStatus{exception: exception}), do: [?;, Serialize.to_ics(exception)]
+end
