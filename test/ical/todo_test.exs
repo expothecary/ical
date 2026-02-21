f5430dd2d6a432407d123501b3445d61c0eb2c6c
Aaron Seigo <aseigo@mykolab.com>
2026-02-21 20:52:01 +0100

    Todo tests

---
+++ test/ical/todo_test.exs
@@ -0,0 +1,26 @@
+defmodule ICal.TodoTest do
+  use ExUnit.Case
+
+  alias ICal.Test.Fixtures
+  alias ICal.Test.Helper
+
+  test "Deserializing calendar with todos" do
+    calendar =
+      "todos"
+      |> Helper.test_data()
+      |> ICal.from_ics()
+
+    assert Enum.count(calendar.todos) == 2
+
+    Enum.each(calendar.todos, fn todo ->
+      assert todo == Fixtures.todo(todo.uid)
+    end)
+  end
+
+  test "Desierializing an empty buffer returns nil" do
+    assert {"", nil} == ICal.Deserialize.Todo.one("", %ICal{})
+  end
+
+  test "Serializing a calendar with todos" do
+  end
+end
