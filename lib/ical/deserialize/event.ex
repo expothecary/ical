2d512ce43291be252b528f1bdf7b05fc28c47da8
Aaron Seigo <aseigo@mykolab.com>
2026-02-21 20:51:34 +0100

    add rejection guards to Component, and next -> next_parameter

--- lib/ical/deserialize/event.ex
+++ lib/ical/deserialize/event.ex
@@ -6,25 +6,25 @@ defmodule ICal.Deserialize.Event do
 
   @spec one(data :: binary, ICal.t()) :: {data :: binary, ICal.Event.t()}
   def one(data, calendar) do
-    next(data, calendar, %ICal.Event{})
+    next_parameter(data, calendar, %ICal.Event{})
   end
 
   Deserialize.Component.parameter_parsers()
 
-  defp next(<<"DTEND", data::binary>>, calendar, event) do
+  defp next_parameter(<<"DTEND", data::binary>>, calendar, event) do
     {data, params} = Deserialize.params(data)
     {data, value} = Deserialize.rest_of_line(data)
     record_value(data, calendar, event, :dtend, Deserialize.to_date(value, params, calendar))
   end
 
-  defp next(<<"TRANSP", data::binary>>, calendar, event) do
+  defp next_parameter(<<"TRANSP", data::binary>>, calendar, event) do
     data = Deserialize.skip_params(data)
     {data, value} = Deserialize.rest_of_line(data)
 
     case value do
-      "OPAQUE" -> next(data, calendar, %{event | transparency: :opaque})
-      "TRANSPARENT" -> next(data, calendar, %{event | transparency: :transparent})
-      _ -> next(data, calendar, event)
+      "OPAQUE" -> next_parameter(data, calendar, %{event | transparency: :opaque})
+      "TRANSPARENT" -> next_parameter(data, calendar, %{event | transparency: :transparent})
+      _ -> next_parameter(data, calendar, event)
     end
   end
 
--- lib/ical/deserialize/todo.ex
+++ lib/ical/deserialize/todo.ex
@@ -4,26 +4,29 @@ defmodule ICal.Deserialize.Todo do
   alias ICal.Deserialize
   require ICal.Deserialize.Component
 
-  @spec one(data :: binary, ICal.t()) :: {data :: binary, ICal.Todo.t()}
+  @spec one(data :: binary, ICal.t()) :: {data :: binary, nil | ICal.Todo.t()}
+
+  Deserialize.Component.rejection_guards()
+
   def one(data, calendar) do
-    next(data, calendar, %ICal.Todo{})
+    next_parameter(data, calendar, %ICal.Todo{})
   end
 
   Deserialize.Component.parameter_parsers()
 
-  defp next(<<"COMPLETED", data::binary>>, calendar, event) do
+  defp next_parameter(<<"COMPLETED", data::binary>>, calendar, event) do
     {data, params} = Deserialize.params(data)
     {data, value} = Deserialize.rest_of_line(data)
     record_value(data, calendar, event, :completed, Deserialize.to_date(value, params, calendar))
   end
 
-  defp next(<<"DUE", data::binary>>, calendar, event) do
+  defp next_parameter(<<"DUE", data::binary>>, calendar, event) do
     {data, params} = Deserialize.params(data)
     {data, value} = Deserialize.rest_of_line(data)
     record_value(data, calendar, event, :due, Deserialize.to_date(value, params, calendar))
   end
 
-  defp next(<<"PERCENT-COMPLETE", data::binary>>, calendar, event) do
+  defp next_parameter(<<"PERCENT-COMPLETE", data::binary>>, calendar, event) do
     data = Deserialize.skip_params(data)
     {data, value} = Deserialize.rest_of_line(data)
     record_integer_value(data, calendar, event, :percent_completed, value)
