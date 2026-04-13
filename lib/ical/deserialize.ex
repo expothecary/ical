3a7c2cb8658fa8bc34a5ae29ead26fe6fc33cc7d
Aaron Seigo <aseigo@yaybr.com>
2026-04-13 09:08:27 +0200

    in gap times, move the time to offset of the gapped time
    gap times return the before/after moments of the gap. our time is
    somewhere in that gap. so shift the after by the amount the gapped
    time would have been after the start

--- lib/ical/deserialize.ex
+++ lib/ical/deserialize.ex
@@ -492,7 +492,7 @@ defmodule ICal.Deserialize do
       case DateTime.new(date, time, timezone) do
         {:ok, dt} -> dt
         {:ambiguous, first, _second} -> first
-        {:gap, _just_before, just_after} -> just_after
+        {:gap, just_before, just_after} -> adjust_to_gap(time, just_before, just_after)
         _ -> nil
       end
     else
@@ -542,4 +542,28 @@ defmodule ICal.Deserialize do
   def status(%ICal.Journal{}, "FINAL"), do: :final
   def status(_, "CANCELLED"), do: :cancelled
   def status(_, _), do: nil
+
+  defp adjust_to_gap(original_time, before_gap, after_gap) do
+    before_gap =
+      before_gap
+      |> DateTime.to_time()
+      |> round_off_micros()
+
+    diff = Time.diff(original_time, before_gap)
+
+    time = Time.shift(DateTime.to_time(after_gap), second: diff)
+
+    DateTime.new!(DateTime.to_date(after_gap), time, after_gap.time_zone)
+  end
+
+  defp round_off_micros(time) do
+    # gap times are often 59.9999 seconds, the moment RIGHT before.
+    # snug those times up to the minute
+    {ms, precision} = time.microsecond
+    offset = round(ms / Integer.pow(10, precision))
+
+    time
+    |> Time.truncate(:second)
+    |> Time.shift(second: offset)
+  end
 end
