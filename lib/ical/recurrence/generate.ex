23b38f79ffae6f787c1e614ef789d37cfc36204d
Aaron Seigo <aseigo@mykolab.com>
2026-04-21 19:47:24 +0200

    move functions of same arity together

--- lib/ical/recurrence/generate.ex
+++ lib/ical/recurrence/generate.ex
@@ -733,14 +733,14 @@ defmodule ICal.Recurrence.Generate do
     end
   end
 
+  defp days_in_month(%Date{} = date), do: Date.days_in_month(date)
+  defp days_in_month(date), do: Date.days_in_month(DateTime.to_date(date))
+
   defp week_of_year(%DateTime{} = datetime), do: week_of_year(DateTime.to_date(datetime))
 
   defp week_of_year(%NaiveDateTime{} = datetime) do
     week_of_year(NaiveDateTime.to_date(datetime))
   end
-
-  defp days_in_month(%Date{} = date), do: Date.days_in_month(date)
-  defp days_in_month(date), do: Date.days_in_month(DateTime.to_date(date))
 
   defp week_of_year(%Date{} = date) do
     end_of_first_week =
