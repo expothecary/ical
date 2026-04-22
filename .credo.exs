e6ee123921c9fec2b9bc5916810444748eca521c
Aaron Seigo <aseigo@mykolab.com>
2026-04-22 11:08:28 +0200

    allow nesting up to 3 levels deep; simply needed for this lib

--- .credo.exs
+++ .credo.exs
@@ -4,6 +4,9 @@
     %{
       name: "default",
       checks: %{
+        extra: [
+          {Credo.Check.Refactor.Nesting, [max_nesting: 3]}
+        ],
         disabled: [
           {Credo.Check.Design.AliasUsage, []}
         ]
