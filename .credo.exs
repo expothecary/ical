be8595cb28c4adac5a04cb42fd838cc2926484ba
Aaron Seigo <aseigo@mykolab.com>
2026-07-06 10:49:30 +0200

    allow functions with great arity than 8

--- .credo.exs
+++ .credo.exs
@@ -5,7 +5,8 @@
       name: "default",
       checks: %{
         extra: [
-          {Credo.Check.Refactor.Nesting, [max_nesting: 3]}
+          {Credo.Check.Refactor.Nesting, [max_nesting: 3]},
+          {Credo.Check.Refactor.FunctionArity, [max_arity: 10]},
         ],
         disabled: [
           {Credo.Check.Design.AliasUsage, []}
