fc2bb84d1729d90342c2ea83430d296d28827d35
Aaron Seigo <aseigo@mykolab.com>
2026-07-06 11:27:10 +0200

    fix triggering test re-runs when ics files change

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,7 @@
 import Config
 
-if Mix.env() == :dev do
-  config :mix_test_watch,
-    extra_extensions: [".ics"]
-end
-
-if Mix.env() == :dev or Mix.env() == :test do
+if Enum.member?([:dev, :test], Mix.env()) do
   config :ical, show_test_timings: false
   config :elixir, :time_zone_database, Tz.TimeZoneDatabase
+  config :mix_test_watch, extra_extensions: [".ics"]
 end
