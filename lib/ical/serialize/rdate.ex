a2ef497c6c9cadde8d01b687b2ac5cadd5a94ef8
Aaron Seigo <aseigo@mykolab.com>
2026-07-08 19:55:41 +0200

    support floating datetimes in RDATES

--- lib/ical/serialize/rdate.ex
+++ lib/ical/serialize/rdate.ex
@@ -7,8 +7,9 @@ defmodule ICal.Serialize.Rdate do
     # reduce the rdates by timezone, so the minimal set of entries gets written out
     # this also separateds out periods, dates, and datetimes as the VALUE= needs to be
     # different for each
-    rdates_by_tz = Enum.reduce(dates, %{}, &by_tz/2)
-    Enum.reduce(rdates_by_tz, acc, &to_rdate_ics/2)
+    dates
+    |> Enum.reduce(%{}, &by_tz/2)
+    |> Enum.reduce(acc, &to_rdate_ics/2)
   end
 
   defp by_tz({from, to}, acc) do
@@ -33,8 +34,20 @@ defmodule ICal.Serialize.Rdate do
     end)
   end
 
+  defp by_tz(%NaiveDateTime{} = date, acc) do
+    serialized = [Serialize.value(date)]
+
+    Map.update(acc, :floating_datetime, serialized, fn dates ->
+      dates ++ serialized
+    end)
+  end
+
   defp to_rdate_ics({:dates, periods}, acc) do
     acc ++ ["RDATE;VALUE=DATE:", Enum.intersperse(periods, ?,), ?\n]
+  end
+
+  defp to_rdate_ics({:floating_datetime, periods}, acc) do
+    acc ++ ["RDATE:", Enum.intersperse(periods, ?,), ?\n]
   end
 
   defp to_rdate_ics({{:periods, "Etc/UTC"}, periods}, acc) do
